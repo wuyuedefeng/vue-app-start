8eb8739dfa9221cb040ae35e5bd1ae24d9c15b63
mars <578595193@qq.com>
2018-04-25 16:22:32 +0800

    .

--- src/components/_ui/index.js
+++ src/components/_ui/index.js
@@ -2,5 +2,10 @@ import UiDialog from '@/components/_ui/_uiDialog'
 export default {
   install (Vue) {
     Vue.use(UiDialog)
+    // button
+    Vue.component('UiButton', (resolve) => {
+      resolve(require('./_uiButton/Button.vue'))
+    })
   }
+
 }
